7f97f13d5f8be7c24c6432fcc0f931a79ce380ff
izumi303 <kanzakiizumi7@gmail.com>
2023-05-03 22:43:02 +0900

    変更

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-//目次を作成する↓↓↓
 document.addEventListener('DOMContentLoaded', () => {
     const heads = document.querySelectorAll('h2');
     if (heads && heads.length) {
         let contents = '';
         heads.forEach((head, i) => {
             let className = '';
-            switch(head.localName) {
-                
+            switch (head.localName) {
+
                 case "h2":
                     className = `contents2`;
                     break;
-               
+
             }
             contents += `<li><a class="${className}" href="#head${i}">${head.textContent}</a></li>`;
             head.innerHTML += `<a id="head${i}"></a>`;
         })
         document.querySelector('#table-of-content').innerHTML += `<ol>${contents}</ol>`;
-
     }
+    //目次作成ここまで
 });
-//↑↑↑目次作成ここまで
